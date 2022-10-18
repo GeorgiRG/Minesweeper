64b287c3c642dd75da54e47e8dad9c16d7f4ef3d
Georgi Georgiev <74297944+GeorgiRG@users.noreply.github.com>
2022-10-18 19:34:44 +0300

    added restart option

--- Minesweeper/Sources/messageboxes.cpp
+++ Minesweeper/Sources/messageboxes.cpp
@@ -5,42 +5,50 @@
 #include <QObject>
 #include <QtGui>
 #include "messageboxes.h"
+#include "game.h"
 #include <iostream>
 
-MessageBoxes::MessageBoxes(QWidget *parent)
+MessageBoxes::MessageBoxes(int gameOver, QWidget *parent)
     : QWidget(parent)
 {
-    gameSize = 0;
-    mineAmount = 0;
+    if(!gameOver) {
+        input1 = new QLineEdit("Specify game size(number), max size is 10(10x10 actual)");
+        input2 = new QLineEdit("Specify mine amount(number), max amount is 99");
+        boxLayout = new QGridLayout(this);
+        startButton = new QPushButton("Start game");
 
-    input1 = new QLineEdit("Specify game size(number), max size is 10");
-    input2 = new QLineEdit("Specify mine amount(number), max amount is 99");
-    boxLayout = new QGridLayout(this);
-    startButton = new QPushButton("Start game");
+        this -> setFixedSize(400, 200);
+        this -> setWindowTitle("New game settings");
 
-    this -> setFixedSize(400, 200);
-    this -> setWindowTitle("New game settings");
+        boxLayout -> addWidget(input1);
 
-    boxLayout -> addWidget(input1);
+        boxLayout -> addWidget(input2);
 
-    boxLayout -> addWidget(input2);
+        boxLayout -> addWidget(startButton);
+        QObject::connect(startButton, SIGNAL(pressed()), this, SLOT(startedGame()));
+        this -> show();
+    }
+    else{
+        this -> setFixedSize(400, 200);
+        this -> setWindowTitle("Game Over!");
+        boxLayout = new QGridLayout(this);
+        startButton = new QPushButton("Press to restart game");
+        boxLayout -> addWidget(startButton);
+        startButton -> setFixedSize(200, 100);
+        QObject::connect(startButton, SIGNAL(pressed()), this, SLOT(restartGame()));
 
+        this -> show();
 
-    boxLayout -> addWidget(startButton);
-    QObject::connect(startButton, SIGNAL(pressed()), this, SLOT(startedGame()));
+    }
 }
 
-void MessageBoxes::RestartGame() {
-    this -> show();
-
-    //int *values[2];
-    //return *values;
-
+void MessageBoxes::restartGame() {
+    game* newGame = new game();
+    this -> close();
 }
 
 void MessageBoxes::startedGame(){
-    this->gameSize = this -> input1->text().toInt();
-    this->mineAmount = this -> input2 -> text().toInt();
-    std::cout << "Hello"<< gameSize << std::endl << mineAmount << std::endl;
-    this->hide();
+    emit startGame(this -> input1->text().toInt(), this -> input2->text().toInt());
+    this -> close();
 }
+
